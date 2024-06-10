5101ebb7072695cdb020ee8145a290bb5796f901
phi_ironman <nguyentruongphi15032003@gmail.com>
2024-06-11 02:31:32 +0700

    category list component

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,7 +3,13 @@ import React from 'react'
 export default function Home() {
   return (
     <div className='bg-gray-200 py-6'>
-      <div className='container'></div>
+      <div className='container grid grid-cols-6'>
+        <div className='col-span-4'>
+          <div className=''></div>
+          <div className=''></div>
+        </div>
+        <div className='col-span-2'></div>
+      </div>
     </div>
   )
 }
--- src/pages/Shop/page/ProductManagement/ProductAdd/ImageItem/ImageItem.tsx
+++ src/pages/Shop/page/ProductManagement/ProductAdd/ImageItem/ImageItem.tsx
@@ -2,10 +2,10 @@ import { useSortable } from '@dnd-kit/sortable'
 import { CSS } from '@dnd-kit/utilities'
 
 interface Props {
-  id: number
+  id: string
   name: string
   url: string
-  deleteImage: (id: number) => void
+  deleteImage: (id: string) => void
 }
 
 export default function ImageItem({ id, name, url, deleteImage }: Props) {
@@ -25,22 +25,15 @@ export default function ImageItem({ id, name, url, deleteImage }: Props) {
     >
       <div className='absolute bottom-0 left-0 w-full h-6 bg-[#333333] hidden group-hover:grid group-hover:grid-cols-2 '>
         <div className='col-span-1'></div>
-        <div
+        <button
           onClick={() => deleteImage(id)}
-          onKeyDown={(event) => {
-            if (event.key === 'Enter' || event.key === ' ') {
-              event.preventDefault()
-              deleteImage(id)
-            }
-          }}
-          tabIndex={0}
-          role='button'
-          className='col-span-1 items-center justify-center flex cursor-pointer'
+          type='button'
+          className='col-span-1 z-20 items-center justify-center flex cursor-pointer'
         >
           <svg xmlns='http://www.w3.org/2000/svg' width='18' height='18' fill='#fff' viewBox='0 0 256 256'>
             <path d='M216,48H176V40a24,24,0,0,0-24-24H104A24,24,0,0,0,80,40v8H40a8,8,0,0,0,0,16h8V208a16,16,0,0,0,16,16H192a16,16,0,0,0,16-16V64h8a8,8,0,0,0,0-16ZM96,40a8,8,0,0,1,8-8h48a8,8,0,0,1,8,8v8H96Zm96,168H64V64H192ZM112,104v64a8,8,0,0,1-16,0V104a8,8,0,0,1,16,0Zm48,0v64a8,8,0,0,1-16,0V104a8,8,0,0,1,16,0Z'></path>
           </svg>
-        </div>
+        </button>
       </div>
       <img className='object-cover h-full w-full' src={url} alt={name} />
     </div>
