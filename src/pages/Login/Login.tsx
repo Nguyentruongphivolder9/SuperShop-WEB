6cab73cf358c69e97e54a6ce552142b176731f80
anh tien <anhtienb237@gmail.com>
2024-07-08 23:19:50 +0700

    Update

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -32,7 +32,7 @@ export default function Login() {
   });
 
   const loginMutation = useMutation({
-    mutationFn: (body: Omit<FormData, 'confirm_password'>) => authApi.login(body)
+    mutationFn: (body: FormData) => authApi.login(body)
   });
 
   const onSubmit = handleSubmit((data) => {
