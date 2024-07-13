59fa6823518dbbec3e4286ed5b5a0fe48c8f35fe
anh tien <anhtienb237@gmail.com>
2024-07-13 15:14:15 +0700

    Final update

--- src/types/user.type.ts
+++ src/types/user.type.ts
@@ -1,14 +1,17 @@
-type Role = 'USER' | 'ADMIN'
+type Role = 'USER' | 'ADMIN';
+
 export interface User {
-  _id: string
-  avatarUrl: string
-  email: string
-  exp: number
-  fullName: string
-  gender: string
-  isActive: string
-  phoneNumber: string
-  role: Role
-  sub: string
-  userName: string
+    _id: string;
+    avatarUrl: string;
+    isActive: boolean;
+    email: string;
+    exp: number;
+    fullName: string;
+    gender: string;
+    birthDay: Date;
+    phoneNumber: string;
+    role: Role;
+    address: string;
+    sub: string;
+    userName: string;
 }
